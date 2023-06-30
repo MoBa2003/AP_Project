7186d32cb2bbe9a671ff8624c01aab9abcacd1fa
Ali <gholamiali41380@gmail.com>
2023-07-01 02:34:12 +0330

    Made Show Error in Login And SignUp Page // SignUp page has a bug in the pass-repeat

--- MainProject/MainProject/LoginForm.xaml.cs
+++ MainProject/MainProject/LoginForm.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MainProject.Controller;
+using MainProject.Models;
+using Npgsql;
 
 namespace MainProject
 {
@@ -37,11 +40,43 @@ namespace MainProject
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+
         }
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            Username_err.Content = "";
+            password_err.Content = "";
+            txtUser.BorderBrush = new SolidColorBrush(Colors.DarkGray);
+            txtPass.BorderBrush = new SolidColorBrush(Colors.DarkGray);
+            if (string.IsNullOrEmpty(txtUser.Text))
+            {
+                Username_err.Content = "Username Invalid";
+                txtUser.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(txtPass.Password))
+            {
+                password_err.Content = "Password Invalid";
+                txtPass.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
 
+            //Employee em = dbFunctions.getEmployeeByUsernamePassword(txtUser.Text, txtPass.Password);
+            //MessageBox.Show(em.Email);
+
+            //dbConnection connection = new dbConnection();
+            //NpgsqlDataReader reader = connection.ExecuteQuery($"select * from tbl_employee where username='{txtUser.Text}' and password='{txtPass.Password}'");
+            //MessageBox.Show(reader.Read().ToString());
+            ////Employee result = new Employee(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6));
+            ////reader.Close();
+
+        }
+
+        private void btn_signupemployee_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            SignUp_Employee window = new SignUp_Employee();
+            window.Show();
+            this.Close();
         }
     }
 }
--- MainProject/MainProject/SignUp_Employee.xaml.cs
+++ MainProject/MainProject/SignUp_Employee.xaml.cs
@@ -1,10 +1,23 @@
 ﻿
 
+using MainProject.Controller;
+using MainProject.Models;
 using System.Text.RegularExpressions;
 using System.Windows;
-
 using System.Windows.Input;
-
+using CustomMessageBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
 
 
 namespace MainProject
@@ -49,6 +62,70 @@ namespace MainProject
 
         private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
+
+        }
+
+        private void btn_signup_Click(object sender, RoutedEventArgs e)
+        {
+            firstname_err.Content = "";
+                //# c5c8cc
+            lastname_err.Content = "";
+            personnelnumber_err.Content = "";
+            Email_err.Content = "";
+            username_err.Content = "";
+            pass_err.Content = "";
+            passrepeat_err.Content = "";
+            txt_firstname.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+            txt_lastname.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+            NumericTextBox.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+            txt_Email.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+            txt_username.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+            txt_password.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+            txt_repeatPassword.BorderBrush = new SolidColorBrush(Colors.WhiteSmoke);
+
+            if (string.IsNullOrEmpty(txt_firstname.Text))
+            {
+                firstname_err.Content = "Firstname Empty";
+                txt_firstname.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(txt_lastname.Text))
+            {
+                lastname_err.Content = "Lastname Empty";
+                txt_lastname.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(NumericTextBox.Text))
+            {
+                personnelnumber_err.Content = "Personnel Number Empty";
+                NumericTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(txt_Email.Text))
+            {
+                Email_err.Content = "Email Empty";
+                txt_Email.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(txt_username.Text))
+            {
+                username_err.Content = "Username Empty";
+                txt_username.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(txt_password.Password))
+            {
+                pass_err.Content = "Password Empty";
+                txt_password.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (string.IsNullOrEmpty(txt_repeatPassword.Password))
+            {
+                passrepeat_err.Content = "PasswordRepeat Empty";
+                txt_repeatPassword.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if(txt_password.Password != txt_repeatPassword.Password)
+            {
+                passrepeat_err.Content = "Does Not Match The Password";
+            }
+
+            // Employee employee = new Employee(null, NumericTextBox.Text, txt_firstname.Text, txt_lastname.Text, txt_Email.Text, txt_username.Text, txt_password.Password);
+            ////MessageBox.Show(dbFunctions.SignUp_Employee(employee).ToString());
+            // new CustomMessageBox.MessageBoxCustom(dbFunctions.SignUp_Employee(employee).ToString(), CustomMessageBox.MessageType.Success, CustomMessageBox.MessageButtons.Ok).Show();
 
         }
     }
